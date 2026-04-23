a64c4f9d581e8a5f0b249943950dff2e5af4156e
MinhTam <tammminh55206@gmail.com>
2026-04-23 13:50:37 +0700

    Sửa lỗi footer và header

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -4,7 +4,7 @@ import { Moon, Sun } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { assetPath } from "@/lib/utils";
 
 /* ============================================
--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true, // ← fix hết tất cả lỗi domain 1 lần
   },
+  basePath: '/nhom09_glowicbeauty',
 };
 
 export default nextConfig;
