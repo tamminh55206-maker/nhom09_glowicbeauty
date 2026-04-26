2f56fbe57b97f6eb0aff138ea67778963403d3f0
Nguyet <luongthinguyet32@gmail.com>
2026-04-26 14:59:38 +0700

    sửa xóa sản phẩm trong giỏ hàng

--- components/cart/CartItems.tsx
+++ components/cart/CartItems.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import { Minus, Plus } from "lucide-react";
+import { Minus, Plus, Trash2 } from "lucide-react";
 import { CartItem } from "@/lib/types";
 
 interface CartItemsProps {
@@ -10,26 +10,26 @@ interface CartItemsProps {
   onRemove: (productId: string) => void;
 }
 
-export default function CartItems({ items, onUpdateQty }: CartItemsProps) {
+export default function CartItems({ items, onUpdateQty,  onRemove }: CartItemsProps) {
   const formatPrice = (price: number) => price.toLocaleString("vi-VN") + "đ";
 
   return (
-    <div className="rounded-2xl bg-white shadow-sm border border-gray-100 overflow-hidden dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-900/20 transition-colors duration-300">
+    <div className=" rounded-2xl bg-white shadow-sm border border-gray-100 overflow-hidden dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-900/20 transition-colors duration-300">
       {/* 1. Header Lưới với các đường kẻ dọc */}
-      <div className="grid grid-cols-12 bg-gray-50 dark:bg-gray-900">
+      <div className="grid grid-cols-13 bg-gray-50 dark:bg-gray-900">
         <div className="col-span-6 p-4 font-medium text-[#000000] dark:text-gray-100 text-sm">Sản phẩm</div>
         <div className="col-span-2 p-4 font-medium text-[#000000] dark:text-gray-100 text-center text-sm ">Giá</div>
         <div className="col-span-2 p-4 font-medium text-[#000000] dark:text-gray-100 text-center text-sm ">Số lượng</div>
-        <div className="col-span-2 p-4 font-medium text-[#000000] dark:text-gray-100 text-center text-sm ">Thành tiền</div>
+        <div className="col-span-3 p-4 mr-12 font-medium text-[#000000] dark:text-gray-100 text-center text-sm ">Thành tiền</div>
       </div>
 
       {/* 2. Danh sách sản phẩm */}
       <div className="divide-y divide-gray-100 dark:divide-gray-700">
         {items.map((item) => (
-          <div key={item.product.id} className="grid grid-cols-12 group hover:bg-gray-50/20 dark:hover:bg-gray-700/40 transition-colors duration-300">
+          <div key={item.product.id} className="grid grid-cols-13 group hover:bg-gray-50/20 dark:hover:bg-gray-700/40 transition-colors duration-300">
             
             
-            <div className="col-span-6 p-5 flex items-center gap-5 border-r border-gray-100 dark:border-gray-700">
+            <div className="col-span-6 p-4 flex items-center gap-4 border-r border-gray-100 dark:border-gray-700">
               <div className="relative h-24 w-24 shrink-0 overflow-hidden rounded-xl border border-gray-100 bg-white dark:border-gray-700 dark:bg-gray-900">
                 <Image 
                   src={item.product.images[0]} 
@@ -50,7 +50,6 @@ export default function CartItems({ items, onUpdateQty }: CartItemsProps) {
                 >
                   {item.product.name}
                 </Link>
-                <p className=" block text-[12px] mt-1.5 text-xs text-[#1F2937] dark:text-gray-300">Màu: {item.product.variant || "01"}</p> 
               </div>
             </div>
 
@@ -82,10 +81,17 @@ export default function CartItems({ items, onUpdateQty }: CartItemsProps) {
             </div>
 
             {/* Cột Thành tiền */}
-            <div className="col-span-2 p-4 flex items-center justify-center text-[15px] font-bold text-[#A53860] dark:text-[#F0A7BC]">
-              {formatPrice(item.product.price * item.quantity)}
-            </div>
+           {/* Cột Thành tiền */}
+<div className=" col-span-3 p-4 flex items-center justify-center text-[15px] font-bold text-[#A53860] dark:text-[#F0A7BC]">
+  {formatPrice(item.product.price * item.quantity)}
 
+  <button
+    onClick={() => onRemove(item.product.id)}
+    className="ml-auto mr-1 text-gray-400 hover:text-[#a53860] transition-colors"
+  >
+    <Trash2 className="h-4 w-4" />
+  </button>
+</div>
           </div>
         ))}
       </div>
